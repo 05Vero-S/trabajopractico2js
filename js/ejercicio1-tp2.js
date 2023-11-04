f1a30c6e75a672214ab0ee4e3d06cd99f7b34264
Vero <vdsoria05@gmail.com>
2023-11-04 11:06:41 -0300

    agregue el ejercicio 1 del tp2, usando condicionales

---
+++ js/ejercicio1-tp2.js
@@ -0,0 +1,14 @@
+//Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
+
+const edad = parseInt(prompt("Ingrese su edad: "));
+
+if(edad >= 18){
+        alert("Usted ya puede conducir");
+    }else if (edad < 18){
+            alert("Usted no puede conducir");
+    }
+else{
+        alert("Ingresó un número inválido")
+    }
+
+
