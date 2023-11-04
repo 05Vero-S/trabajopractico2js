cc86f0e5d4c077f45661e75c7e06c447a07b897e
Vero <vdsoria05@gmail.com>
2023-11-04 12:38:15 -0300

    agregue ejercicio 3 del tp2, usando do while y confirm

---
+++ js/ejercicio3-tp2.js
@@ -0,0 +1,19 @@
+//Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
+
+//Nota: usar confirm() https://www.w3schools.com/jsref/met_win_confirm.asp
+
+let cadenasConcatenadas = ""
+let cadena = prompt("Introduce una cadena: ");
+do{
+    if (cadenasConcatenadas == ""){
+        cadenasConcatenadas = cadenasConcatenadas + cadena;
+    }else{
+        cadenasConcatenadas = cadenasConcatenadas + "-" + cadena;
+    }
+
+}while(confirm("¿Continuar?")){
+    document.write(cadenasConcatenadas);
+}  
+ 
+
+//El método confirm() muestra un cuadro de diálogo con un mensaje, un botón Aceptar y un botón Cancelar.//El método confirm() devuelve verdadero si el usuario hizo click en "Aceptar", en caso contrario devuelve falso.
